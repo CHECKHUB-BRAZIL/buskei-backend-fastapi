120a8a450823bf9b412c12e3e5ce0740bbdf1ad9
sophiasussa <sophiasussa@outlook.com>
2026-01-22 17:27:44 -0300

    fix(middleware): add paths without middleware

---
+++ app/shared/presentation/exempt_paths.py
@@ -0,0 +1,6 @@
+EXEMPT_PATHS = {
+    "/api/v1/auth/login",
+    "/api/v1/auth/register",
+    "/api/v1/auth/refresh",
+    "/api/v1/auth/logout",
+}
--- app/shared/presentation/middlewares/auth_middleware.py
+++ app/shared/presentation/middlewares/auth_middleware.py
@@ -2,22 +2,21 @@ from fastapi import Request
 from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
 
+from app.shared.presentation.exempt_paths import EXEMPT_PATHS
+
 
 class AuthMiddleware(BaseHTTPMiddleware):
 
     async def dispatch(self, request: Request, call_next):
-        session_repo = getattr(
-            request.app.state,
-            "session_repository",
-            None,
-        )
+        # ignora rotas específicas
+        if request.url.path in EXEMPT_PATHS:
+            return await call_next(request)
 
-        # se ainda não estiver pronto (ex: startup)
+        session_repo = getattr(request.app.state, "session_repository", None)
         if session_repo is None:
             return await call_next(request)
 
         auth_header = request.headers.get("Authorization")
-
         if not auth_header or not auth_header.startswith("Bearer "):
             return await call_next(request)
 
