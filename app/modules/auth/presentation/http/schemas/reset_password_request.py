5b8559ec942b8801a871d0200370f8e04acc360a
sophiasussa <sophiasussa@outlook.com>
2026-06-12 15:59:04 -0300

    refactor(auth): simplify reset password request schema

--- app/modules/auth/presentation/http/schemas/reset_password_request.py
+++ app/modules/auth/presentation/http/schemas/reset_password_request.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel, Field
 
 
 class ResetPasswordRequest(BaseModel):
-    token: str = Field(..., min_length=10)
-    new_password: str = Field(..., min_length=8)
+    token: str
+    new_password: str
